ab5c3b61c94348d94988bb271f61aaabbe9212e9
sudhanshu2070 <sssallen2016@gmail.com>
2025-01-11 22:18:05 +0530

    User type Screen 1.7

--- src/screens/UserTypeScreen.tsx
+++ src/screens/UserTypeScreen.tsx
@@ -27,10 +27,10 @@ const UserTypeScreen = () => {
     <View style={styles.container}>
       {/* Top Wave Section */}
       <View style={styles.topSection}>
-        <Svg height="100%" width="100%" viewBox="0 0 1440 320" style={styles.wave}>
+        <Svg height="100%" width="340%" viewBox="180 50 1440 320" style={styles.wave}>
           <Path
             fill="#CCFFCC"
-            d="M0,96L48,101.3C96,107,192,117,288,128C384,139,480,149,576,144C672,139,768,117,864,112C960,107,1056,117,1152,128C1248,139,1344,149,1392,154.7L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+            d='M0,224L48,234.7C96,245,192,267,288,272C384,277,480,267,576,245.3C672,224,768,192,864,181.3C960,171,1056,181,1152,186.7C1248,192,1344,192,1392,192L1440,192L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z'
           />
         </Svg>
         <Image 
@@ -90,6 +90,7 @@ const styles = StyleSheet.create({
   },
   topSection: {
     flex: 0.3,
+    height:'240%',
     position: 'relative',
     justifyContent: 'center',
     alignItems: 'center',
@@ -140,7 +141,7 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 20,
     marginHorizontal: 10,
-    width:'48%',
+    width:'54%',
     height:95,
     justifyContent: 'center', 
     alignItems: 'center',  
